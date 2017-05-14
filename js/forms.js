1998fd99c34bf3f4064089b84bdf0648644af4f9
Valentin Chatelain <val_92_@hotmail.fr>
2017-05-14 22:07:00 +0200

    Started work on multiple bot feature

--- js/forms.js
+++ js/forms.js
@@ -1,109 +1,4 @@
-const countries = [{
-  "text": "UK",
-  "value": "GB"
-}, {
-  "text": "UK (N. IRELAND)",
-  "value": "NB"
-}, {
-  "text": "-",
-  "value": ""
-}, {
-  "text": "AUSTRIA",
-  "value": "AT"
-}, {
-  "text": "BELARUS",
-  "value": "BY"
-}, {
-  "text": "BELGIUM",
-  "value": "BE"
-}, {
-  "text": "BULGARIA",
-  "value": "BG"
-}, {
-  "text": "CROATIA",
-  "value": "HR"
-}, {
-  "text": "CZECH REPUBLIC",
-  "value": "CZ"
-}, {
-  "text": "DENMARK",
-  "value": "DK"
-}, {
-  "text": "ESTONIA",
-  "value": "EE"
-}, {
-  "text": "FINLAND",
-  "value": "FI"
-}, {
-  "text": "FRANCE",
-  "value": "FR"
-}, {
-  "text": "GERMANY",
-  "value": "DE"
-}, {
-  "text": "GREECE",
-  "value": "GR"
-}, {
-  "text": "HUNGARY",
-  "value": "HU"
-}, {
-  "text": "ICELAND",
-  "value": "IS"
-}, {
-  "text": "IRELAND",
-  "value": "IE"
-}, {
-  "text": "ITALY",
-  "value": "IT"
-}, {
-  "text": "LATVIA",
-  "value": "LV"
-}, {
-  "text": "LITHUANIA",
-  "value": "LT"
-}, {
-  "text": "LUXEMBOURG",
-  "value": "LU"
-}, {
-  "text": "MONACO",
-  "value": "MC"
-}, {
-  "text": "NETHERLANDS",
-  "value": "NL"
-}, {
-  "text": "NORWAY",
-  "value": "NO"
-}, {
-  "text": "POLAND",
-  "value": "PL"
-}, {
-  "text": "PORTUGAL",
-  "value": "PT"
-}, {
-  "text": "ROMANIA",
-  "value": "RO"
-}, {
-  "text": "RUSSIA",
-  "value": "RU"
-}, {
-  "text": "SLOVAKIA",
-  "value": "SK"
-}, {
-  "text": "SLOVENIA",
-  "value": "SI"
-}, {
-  "text": "SPAIN",
-  "value": "ES"
-}, {
-  "text": "SWEDEN",
-  "value": "SE"
-}, {
-  "text": "SWITZERLAND",
-  "value": "CH"
-}, {
-  "text": "TURKEY",
-  "value": "TR"
-}];
+const countries = [{"text":"UK","value":"GB"},{"text":"UK (N. IRELAND)","value":"NB"},{"text":"-","value":""},{"text":"AUSTRIA","value":"AT"},{"text":"BELARUS","value":"BY"},{"text":"BELGIUM","value":"BE"},{"text":"BULGARIA","value":"BG"},{"text":"CROATIA","value":"HR"},{"text":"CZECH REPUBLIC","value":"CZ"},{"text":"DENMARK","value":"DK"},{"text":"ESTONIA","value":"EE"},{"text":"FINLAND","value":"FI"},{"text":"FRANCE","value":"FR"},{"text":"GERMANY","value":"DE"},{"text":"GREECE","value":"GR"},{"text":"HUNGARY","value":"HU"},{"text":"ICELAND","value":"IS"},{"text":"IRELAND","value":"IE"},{"text":"ITALY","value":"IT"},{"text":"LATVIA","value":"LV"},{"text":"LITHUANIA","value":"LT"},{"text":"LUXEMBOURG","value":"LU"},{"text":"MONACO","value":"MC"},{"text":"NETHERLANDS","value":"NL"},{"text":"NORWAY","value":"NO"},{"text":"POLAND","value":"PL"},{"text":"PORTUGAL","value":"PT"},{"text":"ROMANIA","value":"RO"},{"text":"RUSSIA","value":"RU"},{"text":"SLOVAKIA","value":"SK"},{"text":"SLOVENIA","value":"SI"},{"text":"SPAIN","value":"ES"},{"text":"SWEDEN","value":"SE"},{"text":"SWITZERLAND","value":"CH"},{"text":"TURKEY","value":"TR"}];
 
 const cards = [{
   "text": "Visa",
@@ -127,87 +22,150 @@ const sizesPants = ["28", "30", "32", "34", "36", "38", "40"];
 
 const currentYear = new Date().getFullYear();
 
+const _preferences = {
+  "autoCheckout": {
+    "type": "boolean",
+    "title": "Auto Checkout",
+    "description": "If enabled, the product will be automatically added to your cart and will proceed to the checkout page",
+    "default": false,
+    "required": true,
+  },
+  "autoPay": {
+    "type": "boolean",
+    "title": "Auto Payment",
+    "description": "Will automatically submit the checkout form when on the checkout page",
+    "default": false,
+    "required": true,
+  },
+  "strictSize": {
+    "type": "boolean",
+    "title": "Strict size choice",
+    "description": "If enabled, the bot will NOT add a product to the cart if your desired size isn't available",
+    "default": false,
+    "required": true,
+  },
+  "captchaBypass": {
+    "type": "boolean",
+    "title": "Bypass Captcha (beta)",
+    "description": "If enabled, the bot will try to bypass the captcha by removing it from the checkout page, not guaranteed to work",
+    "default": false,
+    "required": true,
+  },
+  "hideSoldOut": {
+    "type": "boolean",
+    "title": "Hide sold out products",
+    "description": "If enabled, the sold out products won't be shown on the product list",
+    "default": false,
+    "required": true,
+  },
+  "addToCartDelay": {
+    "type": "integer",
+    "title": "Add to cart delay (ms)",
+    "description": "Delay before adding the product to the cart if autocheckout is enabled",
+    "minimum": 1,
+    "default": 300,
+    "required": true,
+  },
+  "goToCheckoutDelay": {
+    "type": "integer",
+    "title": "Proceed to checkout delay (ms)",
+    "description": "Delay before going to checkout once the product is added to the cart",
+    "minimum": 1,
+    "default": 200,
+    "required": true,
+  },
+  "checkoutDelay": {
+    "type": "integer",
+    "title": "Checkout delay (ms)",
+    "description": "Delay before submitting the payment once on the payment page",
+    "minimum": 1,
+    "default": 1500,
+    "required": true,
+  },
+  "maxPrice": {
+    "type": "integer",
+    "title": "Maximum product price",
+    "description": "Maximum product price, the product will not be added to the cart if it is higher than this price",
+    "minimum": 0,
+    "required": false,
+  },
+  "minPrice": {
+    "type": "integer",
+    "title": "Minimum product price",
+    "description": "Minimum product price, the product will not be added to the cart if it is lower than this price",
+    "minimum": 0,
+    "required": false,
+  },
+};
+
+const _sizings = {
+  "accessories": {
+    "type": "string",
+    "title": "Accessories",
+    "required": true,
+    "minLength": 1,
+    "enum": sizes
+  },
+  "t-shirts": {
+    "type": "string",
+    "title": "T-Shirts",
+    "required": true,
+    "minLength": 1,
+    "enum": sizes
+  },
+  "pants": {
+    "type": "string",
+    "title": "Pants",
+    "required": true,
+    "minLength": 1,
+    "enum": sizesPants
+  },
+  "shorts": {
+    "type": "string",
+    "title": "Shorts",
+    "required": true,
+    "minLength": 1,
+    "enum": sizesPants
+  },
+  "sweatshirts": {
+    "type": "string",
+    "title": "Sweatshirts",
+    "required": true,
+    "minLength": 1,
+    "enum": sizes
+  },
+  "tops": {
+    "type": "string",
+    "title": "Tops/Sweaters",
+    "required": true,
+    "minLength": 1,
+    "enum": sizes
+  },
+  "shirts": {
+    "type": "string",
+    "title": "Shirts",
+    "required": true,
+    "minLength": 1,
+    "enum": sizes
+  },
+  "jackets": {
+    "type": "string",
+    "title": "Jackets",
+    "required": true,
+    "minLength": 1,
+    "enum": sizes
+  },
+};
+
 const _forms = [
   {
+    "category": "Supreme",
     "type": "object",
     "name": "preferences",
-    "properties": {
-      "autoCheckout": {
-        "type": "boolean",
-        "title": "Auto Checkout",
-        "description": "If enabled, the product will be automatically added to your cart and will proceed to the checkout page",
-        "default": false,
-        "required": true,
-      },
-      "autoPay": {
-        "type": "boolean",
-        "title": "Auto Payment",
-        "description": "Will automatically submit the checkout form when on the checkout page",
-        "default": false,
-        "required": true,
-      },
-      "strictSize": {
-        "type": "boolean",
-        "title": "Strict size choice",
-        "description": "If enabled, the bot will NOT add a product to the cart if your desired size isn't available",
-        "default": false,
-        "required": true,
-      },
-      "captchaBypass": {
-        "type": "boolean",
-        "title": "Bypass Captcha (beta)",
-        "description": "If enabled, the bot will try to bypass the captcha by removing it from the checkout page, not guaranteed to work",
-        "default": false,
-        "required": true,
-      },
-      "hideSoldOut": {
-        "type": "boolean",
-        "title": "Hide sold out products",
-        "description": "If enabled, the sold out products won't be shown on the product list",
-        "default": false,
-        "required": true,
-      },
-      "addToCartDelay": {
-        "type": "integer",
-        "title": "Add to cart delay (ms)",
-        "description": "Delay before adding the product to the cart if autocheckout is enabled",
-        "minimum": 1,
-        "default": 300,
-        "required": true,
-      },
-      "goToCheckoutDelay": {
-        "type": "integer",
-        "title": "Proceed to checkout delay (ms)",
-        "description": "Delay before going to checkout once the product is added to the cart",
-        "minimum": 1,
-        "default": 200,
-        "required": true,
-      },
-      "checkoutDelay": {
-        "type": "integer",
-        "title": "Checkout delay (ms)",
-        "description": "Delay before submitting the payment once on the payment page",
-        "minimum": 1,
-        "default": 1500,
-        "required": true,
-      },
-      "maxPrice": {
-        "type": "integer",
-        "title": "Maximum product price",
-        "description": "Maximum product price, the product will not be added to the cart if it is higher than this price",
-        "minimum": 0,
-        "required": false,
-      },
-      "minPrice": {
-        "type": "integer",
-        "title": "Minimum product price",
-        "description": "Minimum product price, the product will not be added to the cart if it is lower than this price",
-        "minimum": 0,
-        "required": false,
-      },
-    }
+    "properties": _preferences
   },
   {
+    "category": "Supreme",
     "type": "object",
     "name": "billing",
     "properties": {
@@ -294,66 +252,22 @@ const _forms = [
     }
   },
   {
+    "category": "Supreme",
     "type": "object",
     "name": "sizings",
-    "properties": {
-      "accessories": {
-        "type": "string",
-        "title": "Accessories",
-        "required": true,
-        "minLength": 1,
-        "enum": sizes
-      },
-      "t-shirts": {
-        "type": "string",
-        "title": "T-Shirts",
-        "required": true,
-        "minLength": 1,
-        "enum": sizes
-      },
-      "pants": {
-        "type": "string",
-        "title": "Pants",
-        "required": true,
-        "minLength": 1,
-        "enum": sizesPants
-      },
-      "shorts": {
-        "type": "string",
-        "title": "Shorts",
-        "required": true,
-        "minLength": 1,
-        "enum": sizesPants
-      },
-      "sweatshirts": {
-        "type": "string",
-        "title": "Sweatshirts",
-        "required": true,
-        "minLength": 1,
-        "enum": sizes
-      },
-      "tops": {
-        "type": "string",
-        "title": "Tops/Sweaters",
-        "required": true,
-        "minLength": 1,
-        "enum": sizes
-      },
-      "shirts": {
-        "type": "string",
-        "title": "Shirts",
-        "required": true,
-        "minLength": 1,
-        "enum": sizes
-      },
-      "jackets": {
-        "type": "string",
-        "title": "Jackets",
-        "required": true,
-        "minLength": 1,
-        "enum": sizes
-      },
-    }
+    "properties": _sizings,
+  },
+  {
+    "category": "Palace",
+    "type": "object",
+    "name": "sizings",
+    "properties": _preferences,
+  },
+  {
+    "category": "Palace",
+    "type": "object",
+    "name": "sizings",
+    "properties": _sizings,
   }
 ];
 
--- js/optionsbuilder.js
+++ js/optionsbuilder.js
@@ -1,15 +1,89 @@
 class OptionsBuilder {
   constructor(forms, targetNode) {
-    this.forms = forms;
     this.targetNode = targetNode;
+    this.forms = forms;
+    this.groupedForms = OptionsBuilder.GroupBy(forms, 'category');
+    this.formCategory = Object.keys(this.groupedForms)[0];
   }
 
   render() {
-    const tabs = OptionsBuilder.CreateTabs(this.forms);
-    const panes = OptionsBuilder.CreatePanes(this.forms);
+    this.targetNode.innerHTML = '';
+    const menu = this.createMenu(this.groupedForms);
 
+    const forms = this.forms.filter(x => x.category === this.formCategory);
+    const tabs = OptionsBuilder.CreateTabs(forms);
+    const panes = OptionsBuilder.CreatePanes(forms);
+
+    this.targetNode.appendChild(menu);
     this.targetNode.appendChild(tabs);
     this.targetNode.appendChild(panes);
+  }
+
+  static GroupBy(xs, key) {
+    return xs.reduce(function (rv, x) {
+      (rv[x[key]] = rv[x[key]] || []).push(x);
+      return rv;
+    }, {});
+  };
+
+  createMenu() {
+    let navContainer = document.createElement('div');
+    navContainer.className = 'navbar navbar-inverse bg-primary';
+
+    let fluidContainer = document.createElement('div');
+    fluidContainer.className = 'container-fluid';
+
+    let navHeader = document.createElement('div');
+    navHeader.className = 'navbar-header';
+
+    navHeader.innerHTML = `
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse-navbar" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+    `;
+
+    let navBrand = document.createElement('a');
+    navBrand.innerText = this.formCategory;
+    navBrand.className = 'navbar-brand';
+
+    let navBarCollapse = document.createElement('div');
+    navBarCollapse.id = 'collapse-navbar';
+    navBarCollapse.className = 'collapse navbar-collapse';
+
+    let navBarUl = document.createElement('div');
+    navBarUl.className = 'nav navbar-nav';
+
+    const keys = Object.keys(this.groupedForms);
+
+    for (let i = 0; i < keys.length; i++) {
+      const group = keys[i];
+      const li = document.createElement('li');
+      li.className = this.formCategory === group ? 'active' : '';
+      const a = document.createElement('a');
+      a.innerText = group;
+      a.href = '#';
+      a.addEventListener('click',() => {
+        this.changeFormCategory(group);
+      });
+
+      li.appendChild(a);
+      navBarUl.appendChild(li);
+    }
+
+    navContainer.appendChild(fluidContainer);
+    fluidContainer.appendChild(navHeader);
+    navHeader.appendChild(navBrand);
+    fluidContainer.appendChild(navBarCollapse);
+    navBarCollapse.appendChild(navBarUl);
+    return navContainer;
+  }
+
+  changeFormCategory(newCategory) {
+    this.formCategory = newCategory;
+    this.render();
   }
 
   static CreateTabs(forms) {
@@ -19,15 +93,16 @@ class OptionsBuilder {
 
     for (let i = 0; i < forms.length; i++) {
       const formName = forms[i].name;
+      const formCategory = forms[i].category || '';
       const li = document.createElement('li');
       li.className = 'nav-item' + (i === 0 ? ' active' : '');
 
       const inner = document.createElement('a');
       inner.className = 'nav-link' + (i === 0 ? ' active' : '');
-      inner.id = `${formName}-form`;
+      inner.id = `${formCategory}-${formName}-form`;
       inner.setAttribute('data-toggle', 'tab');
       inner.setAttribute('role', 'tab');
-      inner.href = `#${formName}-tab`;
+      inner.href = `#${formCategory}-${formName}-tab`;
       inner.innerText = formName;
 
       li.appendChild(inner);
@@ -43,18 +118,20 @@ class OptionsBuilder {
 
     for (let i = 0; i < forms.length; i++) {
       const formName = forms[i].name;
+      const formCategory = forms[i].category || '';
+
       const tabPane = document.createElement('div');
       tabPane.className = 'tab-pane' + (i === 0 ? ' active' : '');
-      tabPane.id = `${formName}-tab`;
+      tabPane.id = `${formCategory}-${formName}-tab`;
       tabPane.setAttribute('role', 'tabpanel');
 
       const inner = document.createElement('div');
-      inner.id = `${formName}-form`;
+      inner.id = `${formCategory}-${formName}-form`;
 
       tabPane.appendChild(inner);
       tabContainer.appendChild(tabPane);
 
-      const form = new FormBuilder(forms[i], inner, formName);
+      const form = new FormBuilder(forms[i], inner, formCategory + '-' + formName);
       form.render();
     }
     return tabContainer;
