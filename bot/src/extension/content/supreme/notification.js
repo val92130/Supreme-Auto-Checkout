427cedf6db30669ddb8f43f7cd542a8ba02c1d29
Valentin Chatelain <val_92_@hotmail.fr>
2017-09-26 22:28:15 +0200

    Update ui

--- bot/src/extension/content/supreme/notification.js
+++ bot/src/extension/content/supreme/notification.js
@@ -1,15 +1,16 @@
 function createNotificationBar() {
   const notificationBar = document.createElement('div');
   notificationBar.style.width = '100%';
+  notificationBar.style.marginLeft = 'auto';
+  notificationBar.style.marginRight = 'auto';
   notificationBar.style.textAlign = 'center';
-  notificationBar.style.backgroundColor = 'rgb(255, 58, 58)';
+  notificationBar.style.backgroundColor = 'rgba(255, 58, 58, 0.42)';
   notificationBar.style.lineHeight = '60px';
   notificationBar.style.height = '60px';
-  notificationBar.style.fontSize = '2em';
+  notificationBar.style.fontSize = '1.6em';
   notificationBar.style.zIndex = '9999';
   notificationBar.style.left = 0;
   notificationBar.style.top = 0;
-  notificationBar.style.fontWeight = 'bold';
   notificationBar.id = 'sup-notif-bar';
   document.body.prepend(notificationBar);
   return notificationBar;
